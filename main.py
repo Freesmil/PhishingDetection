644f707e590e15d8262bdee008782f40037e0f14
Tomas Sevcovic <tomas.sevcovic@cyanrd.cz>
2017-08-02 17:06:42 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+
+
+import ipaddress
+import sys
+from bs4 import BeautifulSoup
+
+
+class Parser:
+
+    def __init__(self, file_name):
+        """
+        Constructor.        
+        :param file_name: string 
+        """
+
+        self.file_name = file_name
+        self.html = self.get_soup_page()
+        self.links = self.get_href_links()
+        self.links_outside = self.get_outside_links()
+
+    def get_soup_page(self):
+        """
+        This method gets DOM (html) and return BeautifulSoup object.
+
+        :return: BeautifulSoup 
+        """
+
+        with open(self.file_name, 'r', encoding="ISO-8859-2") as input_file:
+            html = input_file.read()
+
+        return BeautifulSoup(html, 'html.parser')
+
+    def get_href_links(self):
+        """
+        Gets html DOM and returns links from elements 'a' and 'link' in 'body'.
+
+        :return: set
+        """
+
+        links = []
+
+        for a in self.html.body('a'):
+
+            try:
+                links.append(a['href'])
+            except KeyError:
+                links = []
+
+        for a in self.html.body('link'):
+            try:
+                links.append(a['href'])
+            except KeyError:
+                links = []
+
+        return links
+
+    def get_outside_links(self):
+        """
+        Gets links and return links which are outside of the domain.
+
+        :return: set 
+        """
+
+        outside_links = []
+
+        for link in self.links:
+            if (("https://" in link) or ("http://" in link)) \
+                    and (not (("http://" + sys.argv[3]) in link)
+                         or not (("https://" + sys.argv[3]) in link)
+                         or not (("http://www." + sys.argv[3]) in link)
+                         or not (("https://www." + sys.argv[3]) in link)):
+                outside_links.append(link)
+            else:
+                continue
+
+        return outside_links
+
+
+class LinksModel:
+
+    def __init__(self, link):
+        """
+        
+        
+        :param link: string 
+        """
+
+
+original_page = Parser(sys.argv[1])
+page_after_js = Parser(sys.argv[2])
+
+
+different_links = (set(page_after_js.links_outside) - set(original_page.links_outside))
+
+
+print('--- URL: ', sys.argv[3])
+print('    Total links before: ', len(original_page.links))
+print('    Total links after: ', len(page_after_js.links))
+print('    Outside links before: ', len(original_page.links))
+print('    Outside links after: ', len(original_page.links_outside))
+print('    Differences: ', different_links)
